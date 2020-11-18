64fe8cfbd1210081ac0dec3944d1b3cec72a42de
dzmitrykunitski <dzmitry.kunitski@student.odisee.be>
2020-11-18 11:03:20 +0000

    Changed port from 8080 to 5050.

--- jenkins/sample-app/sample_app.py
+++ jenkins/sample-app/sample_app.py
@@ -5,9 +5,11 @@ from flask import render_template
 
 sample = Flask(__name__)
 
+
 @sample.route("/")
 def main():
     return render_template("index.html")
 
+
 if __name__ == "__main__":
-    sample.run(host="0.0.0.0", port=8080)
+    sample.run(host="0.0.0.0", port=5050)
